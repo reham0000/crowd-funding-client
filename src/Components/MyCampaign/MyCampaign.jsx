9ba2ba72879f41c3548cb0b67196f52e57a197a5
reham0000 <amsarkerreham@gmail.com>
2024-12-13 18:46:14 +0600

    added delete funcnoality

--- src/Components/MyCampaign/MyCampaign.jsx
+++ src/Components/MyCampaign/MyCampaign.jsx
@@ -1,6 +1,9 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider";
+import { MdBrowserUpdated } from "react-icons/md";
+import { MdAutoDelete } from "react-icons/md";
+import Swal from "sweetalert2";
 
 const MyCampaign = () => {
   const myFunds = useLoaderData();
@@ -8,8 +11,47 @@ const MyCampaign = () => {
   const { user } = useContext(AuthContext);
   // console.log(myFunds);
 
+  
+
   const singleUser = myFunds.filter((d) => d?.email == user?.email);
+  const [donation, setDonation] = useState(singleUser);
+  // setDonation(singleUser);
   console.log(singleUser);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+       
+
+        fetch (`http://localhost:5000/myfunds/${id}`, {
+          method: 'DELETE',
+        })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          if(data.deletedCount) {
+             Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+            const remainingDonation = donation.filter(d => d._id !== id);
+            setDonation(remainingDonation)
+          }
+        })
+        
+      }
+      
+    });
+  }
 
   return (
     <>
@@ -17,44 +59,38 @@ const MyCampaign = () => {
         <h1 className="text-5xl font-semibold text-center pt-5">My Campaign</h1>
       </div>
 
-      {singleUser.map((single) => {
-        <div className="overflow-x-auto">
-          <table className="table table-zebra">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>: </th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
-              </tr>
-            </thead>
-            <tbody>
+      <div  className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th> </th>
+              <th>Name</th>
+              <th>Title</th>
+              <th>Amount</th>
+              
+              <th>Deadline</th>
+
+            </tr>
+          </thead>
+
+          {donation.map((single,idx) => (
+            <tbody key={single._id}>
               {/* row 1 */}
               <tr>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Quality Control Specialist</td>
-                <td>Blue</td>
-              </tr>
-              {/* row 2 */}
-              <tr>
-                <th>2</th>
-                <td>Hart Hagerty</td>
-                <td>Desktop Support Technician</td>
-                <td>Purple</td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <th>3</th>
-                <td>Brice Swyre</td>
-                <td>Tax Accountant</td>
-                <td>Red</td>
+                <th>{idx+1}</th>
+                <td>{single.name}</td>
+                <td>{single.title}</td>
+                <td>{single.minDonation}</td>
+                <td>{single.deadline}</td>
+                <td className="text-2xl cursor-pointer"><MdBrowserUpdated></MdBrowserUpdated></td>
+                <td onClick={() => handleDelete (single._id)} className="text-2xl cursor-pointer"><MdAutoDelete></MdAutoDelete></td>
+                
               </tr>
             </tbody>
-          </table>
-        </div>;
-      })}
+          ))}
+        </table>
+      </div>
     </>
   );
 };
